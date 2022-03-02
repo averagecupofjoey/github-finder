91e9d01255e364c220cc7d36698cd448207ce6cd
Joseph Elias <jrelias@outlook.com>
2022-03-02 18:59:17 -0500

    installed axios to deal with api calls

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -6,7 +6,7 @@ import { useParams } from "react-router-dom"
 import RepoList from "../components/repos/RepoList"
 import Spinner from "../components/layout/Spinner"
 import GithubContext from "../context/github/GithubContext"
-import { getUser, getUserRepos } from "../context/github/GithubActions"
+import { getUser, getUserAndRepos, getUserRepos } from "../context/github/GithubActions"
 
 function User() {
 
@@ -17,11 +17,8 @@ function User() {
   useEffect(()=>{
     dispatch({type: "SET_LOADING"})
     const getUserData = async() => {
-      const userData = await getUser(params.login)
-      dispatch({type: "GET_USER", payload: userData})
-
-      const userRepoData = await getUserRepos(params.login)
-      dispatch({type: "GET_REPOS", payload: userRepoData})
+      const userData = await getUserAndRepos(params.login)
+      dispatch({type: "GET_USER_AND_REPOS", payload: userData})
     }
     getUserData()
   }, [])
