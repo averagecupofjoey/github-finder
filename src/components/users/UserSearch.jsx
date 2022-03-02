b92cb4ba53107a1b5c11594bdce998dce0be696e
Joseph Elias <jrelias@outlook.com>
2022-03-02 14:26:39 -0500

    added alert functionality and basic profile information for individual users

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,11 +1,14 @@
 
 import { useState, useContext } from "react"
 import GithubContext from "../../context/github/GithubContext"
+import AlertContext from "../../context/alert/AlertContext"
 
 function UserSearch() {
   const [text, setText] = useState('')
 
-  const {users, searchUsers} = useContext(GithubContext)
+  const {users, searchUsers, clearUsers} = useContext(GithubContext)
+
+  const {setAlert} = useContext(AlertContext)
 
   const handleChange = (e) => setText(e.target.value)
 
@@ -13,7 +16,7 @@ function UserSearch() {
     e.preventDefault()
 
     if(text === ""){
-      alert("Please enter something")
+      setAlert("Please enter something", "error")
     } else {
       searchUsers(text)
       setText('')
@@ -34,11 +37,11 @@ function UserSearch() {
       </div>
       {users.length > 0 && (
       <div>
-        <button className="btn btn-ghost btn-lg">Clear</button>
+        <button onClick={clearUsers} className="btn btn-ghost btn-lg">Clear</button>
       </div>
 
       )}
-      UserSearch</div>
+      </div>
   )
 }
 
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -12,6 +12,7 @@ export const GithubProvider = ({ children }) => {
 
   const initialState = {
     users: [],
+    user: {},
     loading: false,
   };
 
@@ -41,13 +42,48 @@ export const GithubProvider = ({ children }) => {
     });
   };
 
+  // Get single user
+  const getUser = async (login) => {
+    setLoading();
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
+
+    if (response.status === 404) {
+      window.location = '/notfound';
+    } else {
+      const data = await response.json();
+
+      dispatch({
+        type: 'GET_USER',
+        payload: data,
+      });
+    }
+  };
+
   const setLoading = () => {
     dispatch({ type: 'SET_LOADING' });
   };
 
+  const clearUsers = () => {
+    dispatch({
+      type: 'CLEAR_USERS',
+    });
+  };
+
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, searchUsers }}
+      value={{
+        users: state.users,
+        loading: state.loading,
+        user: state.user,
+        searchUsers,
+        getUser,
+        clearUsers,
+      }}
     >
       {children}
     </GithubContext.Provider>
